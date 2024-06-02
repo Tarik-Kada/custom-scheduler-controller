1619dc49bd146dd5abfff6248fe4019e6fa17fad
Tarik-Kada <tarik.kada@student.uva.nl>
2024-06-02 15:11:30 +0200

    Fixed Dockerhub username and image name configurability

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -1,7 +1,10 @@
 # Deploy the custom scheduler controller
+DOCKERHUB_USERNAME="YOUR DOCKERHUB USERNAME"
+IMAGE_NAME="YOUR IMAGE NAME"
+
 echo "Deploying the custom scheduler controller..."
 echo "---------------------------------------------------------------"
-make docker-build docker-push IMG=tarikkada/custom-scheduler-controller:latest
-make deploy IMG=tarikkada/custom-scheduler-controller:latest
+make docker-build docker-push IMG=$DOCKERHUB_USERNAME/$IMAGE_NAME:latest
+make deploy IMG=$DOCKERHUB_USERNAME/$IMAGE_NAME:latest
 echo "The custom scheduler controller has been deployed successfully."
 echo "---------------------------------------------------------------"
